c78182625ef0f2636b5feb1f4919c4c2d56bd789
Alex <36488482+Samael0-0@users.noreply.github.com>
2019-08-17 19:27:42 -0500

    Update Background Changer.cmd

--- Background Changer.cmd
+++ Background Changer.cmd
@@ -1,13 +1,12 @@
 @echo off
-if exist %1 (
-if %~x1==.jpg (
+if not exist %1 ( goto badfile )
+if %~x1 neq .jpg ( goto badfile )
 copy %1 %AppData%\Microsoft\Windows\Themes\TranscodedWallpaper
-for %%F in (%AppData%\Microsoft\Windows\Themes\CachedFiles\*) do (copy %%F %%~nxF & copy %1 %%F & goto break)
+for %%F in ( %AppData%\Microsoft\Windows\Themes\CachedFiles\* ) do ( copy %%F %%~nxF & copy %1 %%F & goto break )
 :break
 echo After you hit a key your computer will restart and your background will be changed or close this and restart later.
 pause
 shutdown -r -t 0
-)
-)
+:badfile
 echo Drag a .jpg file to this file.
 pause
